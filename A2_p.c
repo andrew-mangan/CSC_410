c3b50d7ed1be5617fdabf87530fd9464e5fe0a04
andrew-mangan <andrew.mangan@trojans.dsu.edu>
2024-09-25 19:11:58 -0700

    Add files via upload

--- A2_p.c
+++ A2_p.c
@@ -7,10 +7,7 @@
 int shield_power = 50;  
 
 int main() {
-int Luke_power = shield_power;
-int Han_power = shield_power;
-int Chewy_power = shield_power;
-int Leia_power = shield_power;
+
 
 printf("Millennium Falcon: Initial shield power level: %d%%\n\n", shield_power);
 
@@ -20,26 +17,26 @@ for (int i = 0; i < 4; i++) {
 
                     if (i==0){
                     printf("Luke. Adjusting shield power... \n");
-                    Luke_power += 25;
-                    printf("Luke. power %d. \n", Luke_power);
+                    shield_power += 25;
+                    printf("Luke: Shield power leve now at %d. \n", shield_power);
                     return 0;
                     }
                     else if(i==1){
-                    printf("Han. Adjusting shield power. \n");
-                    Han_power += 20;
-                    printf("Han. power %d.\n", Han_power);
+                    printf("Han: Adjusting shield power. \n");
+                    shield_power += 20;
+                    printf("Han: Shield power leve now at %d.\n", shield_power);
                     return 0;
                     }
                     else if(i==2){
                     printf("Chewbacca. Adjusting shield power.\n");
-                    Chewy_power += 30;
-                    printf("Chewbacca. Power %d \n", Chewy_power);
+                    shield_power += 30;
+                    printf("Chewbacca: Shield power leve now at %d \n", shield_power);
                     return 0;
                     }
                     else if(i==3){
                     printf("Leia. Adjusting shield power. \n");
-                    Leia_power += 25;
-                    printf("Leia. power %d. \n", Leia_power);
+                    shield_power += 25;
+                    printf("Leia: Shield power leve now at %d. \n", shield_power);
                     return 0;
                     }
 
